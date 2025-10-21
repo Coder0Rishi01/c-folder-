6b81893c37586fecba4f89e861cb2fcf47e920cb
Rishikesh Pal <rishikeshpal937@gmail.com>
2025-10-21 09:48:01 +0530

    Chapter 1

---
+++ code.c
@@ -0,0 +1,77 @@
+# include <stdio.h>
+
+                    // OUTPUT OF C 
+
+// int main() {
+//      int age  = 22;
+//      float pi = 3.14;
+//      char hashtag = '#';
+
+//     //printf("Hello word \n");
+
+//     printf("age is %d \n", age);
+//     printf("p is %f \n", pi);
+//     printf("char is %c \n", hashtag);
+   
+//     return 0;
+// }
+
+
+                    // INPUT OF C 
+
+
+// int main() {
+//     int age;
+//     printf("Enter is age :");
+//     scanf("%d", &age);
+//     printf("age is : %d \n", age);
+
+//     return 0;
+// }
+
+
+// Question sum print 
+
+// int main() {
+//     int a,b;
+//     printf("Enter a : ");
+//     scanf("%d", &a);
+
+//     printf("Enter b : ");
+//     scanf("%d", &b);
+
+//     // int sum = a + b;
+//     // printf("Sum is : %d \n", sum);
+//     printf("Sum is : %d \n", a + b);
+
+//     return 0;
+// }
+
+
+
+                        // Practics Qs
+
+// area of square
+
+// int main() {
+//     int side;
+//     printf("enter side : ");
+//     scanf("%d", &side);
+
+//     printf("area is : %d \n", side * side );
+
+//     return 0; 
+// }
+
+
+// area of circle 
+
+int main() {
+    float radius;
+    printf("enter radius : ");
+    scanf("%f", &radius);
+
+    printf("area is : %f \n", 3.14 * radius * radius);
+
+    return 0;
+}
